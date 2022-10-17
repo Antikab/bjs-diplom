7ddbb85a7d29c2a782457ed162b5b6bbd40f740c
Arseny Prosvetov <arsprosvet@mail.ru>
2022-10-17 23:45:34 +0300

    2 правка

--- public/homePage.js
+++ public/homePage.js
@@ -18,3 +18,69 @@ const currentUserApiConnectorCallback = (response) => {
 };
 
 ApiConnector.current(currentUserApiConnectorCallback);
+
+const getStocksApiConnectorCallback = (response) => {
+	if (response.success) {
+		ratesBoard.clearTable();
+		ratesBoard.fillTable(response.data);
+	}
+};
+
+const getRates = () => {
+	ApiConnector.getStocks(getStocksApiConnectorCallback);
+};
+
+getRates();
+setInterval(getRates, 60 * 1000);
+
+const moneyManagerApiConnectorCallback = (response) => {
+	if (response.success) {
+		ProfileWidget.showProfile(response.data);
+		moneyManager.setMessage(true, 'Успешно!');
+	} else {
+		moneyManager.setMessage(false, response.error);
+	}
+};
+
+moneyManager.addMoneyCallback = (data) => {
+	ApiConnector.addMoney(data, moneyManagerApiConnectorCallback);
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+	ApiConnector.convertMoney(data, moneyManagerApiConnectorCallback);
+};
+
+moneyManager.sendMoneyCallback = (data) => {
+	ApiConnector.transferMoney(data, moneyManagerApiConnectorCallback);
+};
+
+const reloadFavoriteList = (data) => {
+	favoritesWidget.clearTable();
+	favoritesWidget.fillTable(data);
+	moneyManager.updateUsersList(data);
+}
+
+const favoritesWidgetApiConnectorCallback = (response) => {
+	if (response.success) {
+		reloadFavoriteList(response.data);
+		favoritesWidget.setMessage(true, 'Действие успешно!');
+	} else {
+		favoritesWidget.setMessage(false, response.error);
+	}
+};
+
+const getFavoritesApiConnectorCallback = (response) => {
+	if (response.success) {
+		reloadFavoriteList(response.data);
+	}
+};
+
+ApiConnector.getFavorites(getFavoritesApiConnectorCallback);
+
+favoritesWidget.addUserCallback = (data) => {
+	ApiConnector.addUserToFavorites(data, favoritesWidgetApiConnectorCallback);
+};
+
+favoritesWidget.removeUserCallback = (data) => {
+	ApiConnector.removeUserFromFavorites(data, favoritesWidgetApiConnectorCallback);
+};
